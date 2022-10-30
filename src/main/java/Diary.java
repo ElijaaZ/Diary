78323fbdb033512e3d02286ce12a31e70b5b36e6
Elias Brunnberg <eliazbrunnberg@outlook.com>
2022-10-30 13:47:00 +0100

    second commit, program finished

---
+++ src/main/java/Diary.java
@@ -0,0 +1,88 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Diary extends Entry {
+
+    private static List<Entry> entriesFromJSON = new ArrayList<>();
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final Path path = Paths.get("src/main/resources/entries.json");
+    private static final Scanner scanner = new Scanner(System.in);
+
+
+    static {
+        try {
+            entriesFromJSON = List.of(mapper.readValue(path.toFile(), Entry[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Metod som skapar ett inlägg och sparar ner det i json
+     */
+    static Entry addNewEntry() {
+        System.out.println("Enter a title");
+        String title = scanner.nextLine();
+        System.out.println("Enter a text");
+        String text = scanner.nextLine();
+
+        String date = LocalDate.now().toString();
+        return new Entry(title, text, date);
+    }
+
+    /**
+     * Metod som skriver ut menyn för användaren
+     */
+    public void menu() {
+        System.out.println("1. Read your entries \n" +
+                "2. Write a new entry \n" +
+                "3. Exit the program");
+    }
+
+    /**
+     * Metod som läser upp entries från JSON
+     */
+    public static void readEntriesFromJSON() throws IOException {
+        List<Entry> entriesFromJSONUPDATED = List.of(mapper.readValue(path.toFile(), Entry[].class));
+        for (Entry entry : entriesFromJSONUPDATED) {
+            showInfoAboutEveryEntry(entry);
+        }
+    }
+
+    /**
+     * Metod som sparar ner din nya entry till JSON
+     * @throws IOException
+     */
+    public static void saveEntryToJSON() throws IOException {
+        entriesFromJSON = List.of(mapper.readValue(path.toFile(), Entry[].class));
+        List<Entry> entries = new ArrayList<>(entriesFromJSON);
+        entries.add(addNewEntry());
+        mapper.writeValue(path.toFile(), entries);
+    }
+
+    /**
+     * Metod som visar informationen om din entry
+     * @param entry
+     */
+    public static void showInfoAboutEveryEntry(Entry entry) {
+        System.out.println("Title: " + entry.getTitle());
+        System.out.println("Text: " + entry.getText());
+        System.out.println("Date: " + entry.getDate());
+
+    }
+
+    /**
+     * Metod som skriver ut ett hejdå meddelande och tackar användaren
+     */
+    public void goodbyeMessage() {
+        System.out.println("Thank you for using the diary, goodbye!");
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+
+    /**
+     * Main metod
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        welcomeMessage();
+
+        Diary diary = new Diary();
+        int choice;
+
+        do {
+            diary.menu();
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    Diary.readEntriesFromJSON();
+                    break;
+                case 2:
+                    Diary.saveEntryToJSON();
+                    break;
+                case 3:
+                    System.out.println("You chose to exit the program, welcome back another time!");
+                    break;
+                default:
+                    System.out.println("Invalid input, pick a number between 1-3");
+            }
+        } while (choice != 3);
+
+        diary.goodbyeMessage();
+
+    }
+
+    /**
+     * Metod som välkomnar användaren
+     */
+    static void welcomeMessage() {
+        System.out.println("Hello and welcome to your diary.");
     }
 }
